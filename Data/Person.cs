32a30580262d69d613ff11bcbfe46d7e2b4eac04
krastio <krastio.dudinov@together.bg>
2021-09-27 17:41:32 +0300

    app

---
+++ Data/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazoredTypeaheadSample.Data
+{
+    public class Person
+    {
+        public Person() { }
+        public Person(int id, string firstname, string lastname, int age, string location)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+            Location = location;
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string FullName { get => Firstname + " " + Lastname; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+    }
+}
